a65b864f3907828d75e0dd2599c4fb2da448aa62
joachim-qi-vinci <joachim.qi@student.vinci.be>
2023-12-03 22:21:47 +0100

    base for stats page

--- frontend/src/Components/Pages/StatsPage.js
+++ frontend/src/Components/Pages/StatsPage.js
@@ -8,7 +8,7 @@ const StatsPage = () => {
 function renderStatsPage(){
   const main = document.querySelector('main');
   main.innerHTML=`
-  <h1 class="text-center" style="padding: 4% ">Statistiques de la partie</h1>
+  <h1 class="text-center" style="padding: 4% ">Statistiques utilisateur</h1>
   <div class='container'>
   
 <table class="table">
@@ -31,6 +31,27 @@ function renderStatsPage(){
     </tr>
   </tbody>
 </table>
+</div>
+
+<div class="card text-center">
+  <div class="card-header">
+    <ul class="nav nav-tabs card-header-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
 </div>
 `
 
