03ae1bec34938cc140b6bc8f9cce5b6b0378c63e
jason-chu-vinci <jason.chu@student.vinci.be>
2023-12-05 11:27:42 +0100

    add StatPage router

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -4,6 +4,7 @@ import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
 import SetUpPage from '../Pages/SetUpPage';
 import PersonnalStatsPage from '../Pages/PersonnalStatsPage';
+import StatsPage from '../Pages/StatPage';
 
 const routes = {
   '/': HomePage,
@@ -11,7 +12,8 @@ const routes = {
   '/connexion' : LoginPage,
   '/setup': SetUpPage,
   '/inscription' : RegisterPage,
-  '/stats' : PersonnalStatsPage
+  '/stats' : PersonnalStatsPage,
+  '/gameStats' : StatsPage
 };
 
 export default routes;
