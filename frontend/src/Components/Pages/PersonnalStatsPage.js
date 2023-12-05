bfafa7be97faed6d5a9f845796200c3e8fbd6f8d
joachim-qi-vinci <joachim.qi@student.vinci.be>
2023-12-05 11:40:07 +0100

    updated sql script

--- frontend/src/Components/Pages/PersonnalStatsPage.js
+++ frontend/src/Components/Pages/PersonnalStatsPage.js
@@ -10,7 +10,17 @@ function renderPersonnalStatsPage(){
   main.innerHTML=`
   <h1 class="text-center" style="padding: 4% ">Statistiques utilisateur</h1>
   <div class='container'>
-  
+
+  <div class = "container mb-5">
+<div class="card w-50 d-flex">
+  <div class="card-body">
+    <span id = "nom_utilisateur"></span>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Button</a>
+  </div>
+  </div>
+</div>
+
 <table class="table">
   <thead>
     <tr>
@@ -32,30 +42,8 @@ function renderPersonnalStatsPage(){
   </tbody>
 </table>
 </div>
-
-<div class="card text-center">
-  <div class="card-header">
-    <ul class="nav nav-tabs card-header-tabs">
-      <li class="nav-item">
-        <a class="nav-link active" href="#">Active</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
 </div>
 `
-
 }
-
 
 export default PersonnalStatsPage;
