601296b7095530be32e9bd84b988acb649ee4bf9
joachim-qi-vinci <joachim.qi@student.vinci.be>
2023-12-06 18:57:48 +0100

    merge api categories

--- api/models/categories.js
+++ api/models/categories.js
@@ -2,10 +2,9 @@ const client = require('../elephantsql');
 
 async function readAllCategories() {
   const result = await client.query('SELECT DISTINCT c.nom_categorie FROM web2.categories c');
-  console.log(result);
   return result.rows;
 }
 
 module.exports = {
-  readAllCategories, getOneCategory,
+  readAllCategories,
 };
--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,21 +1,13 @@
-/* eslint-disable camelcase */
-/* eslint-disable consistent-return */
 const express = require('express');
-const { readAllCategories, getOneCategory } = require('../models/categories');
+
+const {
+  readAllCategories,
+} = require('../models/categories');
 
 const router = express.Router();
-router.get('/', async (req, res) => {
-  const category = req?.query?.category;
-  if (!category) {
-    return res.sendStatus(400);
-  }
-  const response = await getOneCategory(category);
-  return res.json(response);
-});
 
 // get all the categories
 router.get('/', async (req, res) => {
-  console.log('coucoiu');
   const categories = await readAllCategories();
 
   if (!categories) {
