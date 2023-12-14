2b547ac12c744d162df4ce72262f544d88dfc601
joachim-qi-vinci <joachim.qi@student.vinci.be>
2023-12-14 11:26:52 +0100

    attemp to render game page & wheel

--- frontend/src/Components/Pages/GamePage.js
+++ frontend/src/Components/Pages/GamePage.js
@@ -1,24 +1,65 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 
 import { clearPage } from '../../utils/render';
+import getOneQuestion from '../../models/question';
 
 const GamePage = async () => {
   clearPage();
+  
   renderGamePage();
+ 
   renderQuestion();
 };
 
 function renderGamePage(){
-    const main = document.querySelector('main');
-    main.innerHTML = `
-    
-    <div class="mb-5" style = "text-align:center;">
-        <canvas id="canvas" width="500" height="500"></canvas>
-        <input type="button" value="spin" style="float:center;" id='spin' />
+  const main = document.querySelector('main');
+    main.innerHTML =`
+    <div class="container" id="questionWrapper" style: "z-index: 10"></div>
+    <div class="position relative p-5">
+        <div class="position-absolute top-0 start-0 mt-5">
+            <div class="card mt-4">
+                <div class = "card-body bg-info">
+                    <h5 id="player1Name" class="card-title">Joueur 1 </h5>
+                </div>
+            </div>
+        </div>
+        <div class="position-absolute top-0 end-0 mt-5">
+            <div class="card mt-4">
+                <div class = "card-body bg-success">
+                    <h5 id="player2Name" class="card-title">Joueur 2 </h5>
+                </div>
+            </div>
+        </div>
+        <div id="player3Card" class="position-absolute bottom-0 start-0">
+            <div class="card">
+                <div class = "card-body bg-danger">
+                    <h5 id="player3Name" class="card-title">Joueur 3 </h5>
+                </div>
+            </div>
+        </div>
+        <div id="player4Card" class="position-absolute bottom-0 end-0">
+            <div class="card mt-4">
+                <div class = "card-body bg-warning">
+                    <h5 id="player4Name" class="card-title">Joueur 4 </h5>
+                </div>
+            </div>
+        </div>
     </div>
+    ` 
+  renderRoulette();
+    players()
+}
 
-    `
-    renderWheel();
+function renderRoulette(){
+  const main = document.querySelector('main');
+  main.innerHTML =`
+  
+  <div class="mb-5" style = "text-align:center;">
+      <canvas id="canvas" width="500" height="500"></canvas>
+      <input type="button" value="spin" style="float:center;" id='spin' />
+  </div>
+<input type="button" value="spin" style="float:center;" id='spin' />`
+renderWheel();
 }
 
 function renderWheel(){
@@ -160,43 +201,8 @@ function renderQuestion(){
   `
   renderAQuestion() 
 }
-function renderGamePage(){
-  const main = document.querySelector('main');
-    main.innerHTML =`
-    <div class="container" id="questionWrapper" style: "z-index: 10"></div>
-    <div class="position relative p-5">
-        <div class="position-absolute top-0 start-0 mt-5">
-            <div class="card mt-4">
-                <div class = "card-body bg-info">
-                    <h5 id="player1Name" class="card-title">Joueur 1 </h5>
-                </div>
-            </div>
-        </div>
-        <div class="position-absolute top-0 end-0 mt-5">
-            <div class="card mt-4">
-                <div class = "card-body bg-success">
-                    <h5 id="player2Name" class="card-title">Joueur 2 </h5>
-                </div>
-            </div>
-        </div>
-        <div id="player3Card" class="position-absolute bottom-0 start-0">
-            <div class="card">
-                <div class = "card-body bg-danger">
-                    <h5 id="player3Name" class="card-title">Joueur 3 </h5>
-                </div>
-            </div>
-        </div>
-        <div id="player4Card" class="position-absolute bottom-0 end-0">
-            <div class="card mt-4">
-                <div class = "card-body bg-warning">
-                    <h5 id="player4Name" class="card-title">Joueur 4 </h5>
-                </div>
-            </div>
-        </div>
-    </div>
-    ` 
-    players()
-}
+
+
 function players(){
     const player1Name = document.querySelector("#player1Name");
     player1Name.innerHTML = `HEllo`
