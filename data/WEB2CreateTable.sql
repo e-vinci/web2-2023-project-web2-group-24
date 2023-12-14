61bb7e28af9b04789298955615fafe4d2f3302de
joachim-qi-vinci <joachim.qi@student.vinci.be>
2023-12-14 23:10:46 +0100

    update insert question

--- data/WEB2CreateTable.sql
+++ data/WEB2CreateTable.sql
@@ -1,17 +1,17 @@
 DROP SCHEMA IF EXISTS web2 CASCADE ;
 CREATE SCHEMA web2;
 
-CREATE TABLE web2.utilisateurs
-(
-    no_utilisateur  SERIAL PRIMARY KEY NOT NULL,
-    nom_utilisateur VARCHAR(30)      NOT NULL UNIQUE
-        CHECK ( utilisateurs.nom_utilisateur <> '' ),
-    email           VARCHAR(60)        NOT NULL UNIQUE
-        CHECK ( utilisateurs.email SIMILAR TO '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]+'),
-    mdp             VARCHAR(100)       NOT NULL
-        CHECK ( utilisateurs.mdp <> '' )
+CREATE TABLE web2.utilisateurs(
+                                  no_utilisateur SERIAL PRIMARY KEY NOT NULL,
+                                  nom VARCHAR(30) NOT NULL
+                                      CHECK ( utilisateurs.nom <> '' ),
+                                  prenom VARCHAR(30) NOT NULL
+                                      CHECK ( utilisateurs.prenom<> '' ),
+                                  email VARCHAR(60) NOT NULL UNIQUE
+                                      CHECK ( utilisateurs.email SIMILAR TO '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]+'),
+                                  mdp VARCHAR(60) NOT NULL
+                                      CHECK ( utilisateurs.mdp <> '' )
 );
-
 
 CREATE TABLE web2.categories(
                                 id_categorie CHAR(4) PRIMARY KEY NOT NULL
@@ -40,19 +40,11 @@ CREATE TABLE web2.questions(
 
 
 INSERT INTO web2.categories(id_categorie, nom_categorie) VALUES ('INFO', 'INFORMATIQUE');
-INSERT INTO web2.categories(id_categorie, nom_categorie) VALUES ('DIET', 'DIETETIQUE');
-INSERT INTO web2.categories(id_categorie, nom_categorie) VALUES ('INFI', 'INFIRMIER');
-INSERT INTO web2.categories(id_categorie, nom_categorie) VALUES ('EDPH', 'EDUCATION PHYSIQUE');
+INSERT INTO web2.categories(id_categorie, nom_categorie) VALUES ('DIET', 'DIETETIQUE');--
+INSERT INTO web2.categories(id_categorie, nom_categorie) VALUES ('INFI', 'INFIRMIER');--
+INSERT INTO web2.categories(id_categorie, nom_categorie) VALUES ('COSP', 'COACHING SPORTIF');
 INSERT INTO web2.categories(id_categorie, nom_categorie) VALUES ('IMGM', 'IMAGERIE MEDICALE');
 INSERT INTO web2.categories(id_categorie, nom_categorie) VALUES ('ENSE', 'ENSEIGNANT');
-
-INSERT INTO web2.utilisateurs(nom_utilisateur, email, mdp) VALUES ('Robin', 'greg.laplume@gmail.com','$2b$10$0uPAIotQVk3n2uQm0d.n0ukJH8zoui8S9mAbUjDHa5fePDNPVyEfO');
-INSERT INTO web2.statistiques(utilisateur, nb_questions_posees, nb_parties_jouees, nb_victoire, categorie_preferee) VALUES (1,10,5,2,'ENSE');
-
-
-
-INSERT INTO web2.utilisateurs(nom, prenom, email, mdp) VALUES ('QI', 'Jo', 'joachim.qi@gmail.com', '1244');
-INSERT INTO web2.statistiques(utilisateur, nb_parties_jouees, categorie_preferee, nb_victoire) VALUES (1, 10 ,'INFO', 10);
 
 
 INSERT INTO web2.questions(categorie, question, valeur) VALUES ('ENSE','Après avoir fini les 3 années de bachelier en instituteur primaire, nous sommes également diplomé comme maître spécial de religion',TRUE);
@@ -66,52 +58,28 @@ INSERT INTO web2.questions(categorie, question, valeur) VALUES ('ENSE','Dans cet
 INSERT INTO web2.questions(categorie, question, valeur) VALUES ('ENSE','Dans cette formation, il n''y a que deux stages en BAC3' , FALSE);
 INSERT INTO web2.questions(categorie, question, valeur) VALUES ('ENSE','Dans cette formation, durant le BAC2 il y a un mois de stage où 80% de l''horaire d''une classe est pris en charge par un étudiant',TRUE);
 INSERT INTO web2.questions(categorie, question, valeur) VALUES ('ENSE','Dans cette formation, il n''y pas de cours de musique et d''art plastique. Ces cours sont réservés uniquement aux cursus d''instituteur maternelle',FALSE);
-INSERT INTO web2.questions(categorie, question, valeur) VALUES ('IMGM', 'La radiographie est une modalité d’imagerie qui utilise des ondes sonores pour créer des images du corps humain', FALSE);
-INSERT INTO web2.questions(categorie, question, valeur) VALUES ('IMGM', 'Les rayons x sont utilisés dans une tomographie par ordinateur', TRUE);
-INSERT INTO web2.questions(categorie, question, valeur) VALUES ('IMGM', 'Une IRM est une modalité d’imagerie utilisant un champ magnétiqque  et des ondes radio pour créer des images détaillées du corps', TRUE);
-INSERT INTO web2.questions(categorie, question, valeur) VALUES ('IMGM', 'Les rayons ultraviolets sont utilisés dans la spectroscopie par résonance magnétique ', FALSE);
-INSERT INTO web2.questions(categorie, question, valeur) VALUES ('IMGM', 'On utilise les Rayons gamma pour détecter la radioactivité du corps.', TRUE);
-INSERT INTO web2.questions(categorie, question, valeur) VALUES ('IMGM', 'L’Angiographie est utilisée pour détecter les vaisseaux sanguins', TRUE);
-INSERT INTO web2.questions(categorie, question, valeur) VALUES ('IMGM', 'Une échographie est souvent utilisée pour l’évaluation du métabolisme cellulaire', FALSE);
-INSERT INTO web2.questions(categorie, question, valeur) VALUES ('IMGM', 'Une radiographie est particulièrement utile pour détecter les fractures osseuses', TRUE);
 
 
-SELECT u.* FROM web2.utilisateurs u WHERE u.nom_utilisateur = 'gfzefrzef';
-/*
-CREATE OR REPLACE FUNCTION web2.ajout_utilisateur() RETURN VOID AS $$
-DECLARE
-    no_utilisateur INTEGER;
-BEGIN
-    SELECT u.no_utilisateur FROM web2.utilisateurs u WHERE u.email = mail INTO no_utilisateur;
-    IF (no_utilisateur !== NULL) THEN
-    UPDATE web2.statistiques SET nb_questions_posees = nb_questions_posees + nb_question WHERE web2.statistiques.utilisateur = no_utilisateur;
-    END IF;
-    END;
-$$ LANGUAGE plpgsql;
 
 
-CREATE OR REPLACE FUNCTION web2.trigger_ajout_nb_questions() RETURNS TRIGGER AS $$
-BEGIN
-    IF (NEW.nb_questions_posees <= 0) THEN
-        RAISE 'nombre de questions posées non valide';
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Le terme « bug » en informatique vient d''insectes qui faisaient griller les lampes des premiers ordinateurs', TRUE );
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','En programmation une chaine de charactères (un texte) est appelée « String »', TRUE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Le software est l''ensemble des éléments matériels d''un système informatique', FALSE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Le langage assembleur est un langage de programmation de haut niveau.', FALSE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','1 Gigaoctet est égal à 1000 Megaoctet.', FALSE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','On trouve des systèmes d''exploitation dans les avions', TRUE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Le premier ordinateur pesait 50 tonnes', TRUE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Le nombre 3 s''écrit 0100 en binaire', FALSE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Le langage de programmation le plus populaire en 2023 est Python', FALSE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Le langage de requêtes SQL est principalement utiliser pour manipuler des bases de données relationnelles.', TRUE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','A la Haute École, le cours d’APOO signifie « Analyse et programmation orienté ordonnanceur »', FALSE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Le langage de programmation Java doit son nom au café que beaucoup de développeurs apprécient ', TRUE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','La Commission nationale de l''informatique et des libertés (CNIL) a condamné l''entreprise Google à payer deux amendes d''un montant total de 100 millions d''euros pour non-respect des règles RGPD', TRUE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Les polices d’écriture sans-serif facilitent la lecture aux mal-voyants', FALSE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Les requêtes post servent à envoyer des données au serveur', TRUE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Le format fichier PNG est principalement utilisé pour des fichiers audio compressés', FALSE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Un email génère environ 4g de CO2', TRUE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','La consommation de vidéos en streaming génère 300 millions de tonnes de CO2 par an ', TRUE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','TDD signifie To Do Documentation', FALSE);
+INSERT INTO web2.questions(categorie, question, valeur) values ('INFO','Un framework est un schéma des différentes pages d’un site web ', FALSE);
 
-CREATE TRIGGER trigger_ajout_nb_questions BEFORE UPDATE ON web2.statistiques
-    FOR EACH ROW EXECUTE PROCEDURE web2.trigger_ajout_nb_questions();
-
-
-CREATE OR REPLACE FUNCTION web2.ajouter_nb_questions(nb_question INTEGER, mail INTEGER) RETURNS VOID AS $$
-DECLARE
-    no_utilisateur INTEGER;
-BEGIN
-    SELECT u.no_utilisateur FROM web2.utilisateurs u WHERE u.email = mail INTO no_utilisateur;
-    IF (no_utilisateur !== NULL) THEN
-    UPDATE web2.statistiques SET nb_questions_posees = nb_questions_posees + nb_question WHERE web2.statistiques.utilisateur = no_utilisateur;
-    END IF;
-    END;
-$$ LANGUAGE plpgsql;
-
-*/
