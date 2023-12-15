647c35b59ae77fdd34650847ccfe958e1fe1bbde
margjini <mario.margjini@student.vinci.be>
2023-12-15 21:02:22 +0100

    add stars on gamepage

--- frontend/src/Components/Pages/GamePage.js
+++ frontend/src/Components/Pages/GamePage.js
@@ -27,6 +27,8 @@ function renderGamePage(){
                 <div class="card mt-4">
                     <div class = "card-body bg-info">
                         <h5 id="player1Name" class="card-title">Joueur 1 </h5>
+                        <div id='stars1'>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -34,6 +36,8 @@ function renderGamePage(){
                 <div class="card mt-4">
                     <div class = "card-body bg-success">
                         <h5 id="player2Name" class="card-title">Joueur 2 </h5>
+                        <div id='stars2'>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -41,6 +45,8 @@ function renderGamePage(){
                 <div class="card">
                     <div class = "card-body bg-danger">
                         <h5 id="player3Name" class="card-title">Joueur 3 </h5>
+                        <div id='stars3'>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -48,12 +54,14 @@ function renderGamePage(){
                 <div class="card mt-4">
                     <div class = "card-body bg-warning">
                         <h5 id="player4Name" class="card-title">Joueur 4 </h5>
+                        <div id='stars4'>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    }
     ` 
+    stars()
     players()
     renderWheel()
     renderTurn()
@@ -189,26 +197,205 @@ function players(){
   const player1Name = document.querySelector("#player1Name");
   const player1 = JSON.parse(sessionStorage.getItem('player1'));
   player1Name.innerHTML = player1.name;
+  if(player1.answerINFO>0){
+    const starINFO1 = document.querySelector("#starINFO1");
+    starINFO1.style.color= "Salmon"
+  }
+  if(player1.answerDIET>0){
+    const starDIET1 = document.querySelector("#starDIET1");
+    starDIET1.style.color= "Thistle"
+  }
+  if(player1.answerINFI>0){
+    const starINFI1 = document.querySelector("#starINFI1");
+    starINFI1.style.color= "Yellow"
+  }
+  if(player1.answerCOSP>0){
+    const starCOSP1 = document.querySelector("#starCOSP1");
+    starCOSP1.style.color= "LimeGreen"
+  }
+  if(player1.answerIMGM>0){
+    const starIMGM1 = document.querySelector("#starIMGM1");
+    starIMGM1.style.color= "SkyBlue"
+  }
+  if(player1.answerENSE>0){
+    const starENSE1 = document.querySelector("#starENSE1");
+    starENSE1.style.color= "violet"
+  }
   
   const player2Name = document.querySelector("#player2Name");
   const player2 = JSON.parse(sessionStorage.getItem('player2'))
   player2Name.innerHTML = player2.name;
+  if(player2.answerINFO>0){
+    const starINFO2 = document.querySelector("#starINFO2");
+    starINFO2.style.color= "Salmon"
+  }
+  if(player2.answerDIET>0){
+    const starDIET2 = document.querySelector("#starDIET2");
+    starDIET2.style.color= "Thistle"
+  }
+  if(player2.answerINFI>0){
+    const starINFI2 = document.querySelector("#starINFI2");
+    starINFI2.style.color= "Yellow"
+  }
+  if(player2.answerCOSP>0){
+    const starCOSP2 = document.querySelector("#starCOSP2");
+    starCOSP2.style.color= "LimeGreen"
+  }
+  if(player2.answerIMGM>0){
+    const starIMGM2 = document.querySelector("#starIMGM2");
+    starIMGM2.style.color= "SkyBlue"
+  }
+  if(player2.answerENSE>0){
+    const starENSE2 = document.querySelector("#starENSE2");
+    starENSE2.style.color= "violet"
+  }
   const player3Name = document.querySelector("#player3Name");
   const player3 = JSON.parse(sessionStorage.getItem('player3'));
   if (player3 === null){
     player3Name.innerHTML = "Joueur 3"
+    const player3Stars = document.querySelector("#stars3");
+    player3Stars.innerHTML='';
   }else{
     player3Name.innerHTML = player3.name
+    if(player3.answerINFO>0){
+      const starINFO3 = document.querySelector("#starINFO3");
+      starINFO3.style.color= "Salmon"
+    }
+    if(player3.answerDIET>0){
+      const starDIET3 = document.querySelector("#starDIET3");
+      starDIET3.style.color= "Thistle"
+    }
+    if(player3.answerINFI>0){
+      const starINFI3 = document.querySelector("#starINFI3");
+      starINFI3.style.color= "Yellow"
+    }
+    if(player3.answerCOSP>0){
+      const starCOSP3 = document.querySelector("#starCOSP3");
+      starCOSP3.style.color= "LimeGreen"
+    }
+    if(player3.answerIMGM>0){
+      const starIMGM3 = document.querySelector("#starIMGM3");
+      starIMGM3.style.color= "SkyBlue"
+    }
+    if(player3.answerENSE>0){
+      const starENSE3 = document.querySelector("#starENSE3");
+      starENSE3.style.color= "violet"
+    }
   }
   
   const player4Name = document.querySelector("#player4Name");
   const player4 = JSON.parse(sessionStorage.getItem('player4'))
   if (player4 === null){
     player4Name.innerHTML = "Joueur 4"
+    const player4Stars = document.querySelector("#stars4");
+    player4Stars.innerHTML='';
   }else{
     player4Name.innerHTML = player4.name
+    
+    if(player4.answerINFO>0){
+      const starINFO4 = document.querySelector("#starINFO4");
+      starINFO4.style.color= "Salmon"
+    }
+    if(player4.answerDIET>0){
+      const starDIET4 = document.querySelector("#starDIET4");
+      starDIET4.style.color= "Thistle"
+    }
+    if(player4.answerINFI>0){
+      const starINFI4 = document.querySelector("#starINFI4");
+      starINFI4.style.color= "Yellow"
+    }
+    if(player4.answerCOSP>0){
+      const starCOSP4 = document.querySelector("#starCOSP4");
+      starCOSP4.style.color= "LimeGreen"
+    }
+    if(player4.answerIMGM>0){
+      const starIMGM4 = document.querySelector("#starIMGM4");
+      starIMGM4.style.color= "SkyBlue"
+    }
+    if(player4.answerENSE>0){
+      const starENSE4 = document.querySelector("#starENSE4");
+      starENSE4.style.color= "violet"
+    }
   }
+}
 
+function stars(){
+  const starsPlayer1 = document.querySelector("#stars1");
+  starsPlayer1.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starINFO1'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starCOSP1'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starIMGM1'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starDIET1'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id='starINFI1'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id='starENSE1'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>`
+  const starsPlayer2 = document.querySelector("#stars2");
+  starsPlayer2.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starINFO2'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starCOSP2'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starIMGM2'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starDIET2'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id='starINFI2'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id='starENSE2'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>`
+  const starsPlayer3 = document.querySelector("#stars3");
+  starsPlayer3.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starINFO3'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starCOSP3'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starIMGM3'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starDIET3'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id='starINFI3'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id='starENSE3'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>`
+  const starsPlayer4 = document.querySelector("#stars4");
+  starsPlayer4.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starINFO4'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starCOSP4'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starIMGM4'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id ='starDIET4'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id='starINFI4'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" id='starENSE4'>
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>`
 }
 
 
