d0879929fc78d41825227d1c30d2e3c594dd72a1
jason-chu-vinci <jason.chu@student.vinci.be>
2023-12-16 17:31:23 +0100

    resolved commment errors

--- frontend/src/Components/Pages/GamePage.js
+++ frontend/src/Components/Pages/GamePage.js
@@ -71,15 +71,14 @@ function renderGamePage(){
 
 
 
-
-/*************************************************************************************** 
-*    Title: <Roulette Wheel> 
-*    Author: <Barney Parker> 
-*    Date: <December 4, 2014> 
-*    Code version: <1.0> 
-*    Availability: <https://codepen.io/barney-parker/pen/OPyYqy> 
-* 
-***************************************************************************************/ 
+// ************************************************************************************* 
+// Title: <Roulette Wheel> 
+// Author: <Barney Parker> 
+// Date: <December 4, 2014> 
+// Code version: <1.0> 
+// Availability: <https://codepen.io/barney-parker/pen/OPyYqy> 
+// 
+// *************************************************************************************/
 function renderWheel(){
     const options = ['INFO', 'DIET', "INFI", 'COSP', 'IMGM', 'ENSE'];
 
